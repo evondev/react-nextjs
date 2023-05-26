0ceda8de2195749d119c3a6912b892984bb57862
evondev <trananhtuan400@gmail.com>
2023-05-26 10:36:45 +0700

    restore

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,5 +2,5 @@ import { LayoutMain } from "@/components/layout";
 import ImageComponent from "@/components/images/ImageComponent";
 
 export default function Home() {
-  return <ImageComponent></ImageComponent>;
+  return <LayoutMain>home</LayoutMain>;
 }
