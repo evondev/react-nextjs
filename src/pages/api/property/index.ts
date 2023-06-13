62f54f75443f296be8f3c387dcec041fb93cb1ec
evondev <trananhtuan400@gmail.com>
2023-06-13 15:07:44 +0700

    update

--- src/pages/api/property/index.ts
+++ src/pages/api/property/index.ts
@@ -2,12 +2,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 import properties from "@data/properties.json";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  // const params = req.query;
   let filteredData = properties;
-
-  // if (filter1) {
-  //   filteredData = filteredData.filter(item => item.category === filter1);
-  // }
-
   res.status(200).json(filteredData);
 }
